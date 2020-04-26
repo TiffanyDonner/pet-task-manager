32902c1dddf9c88e71d59744889170a5d9f4924d
Tiffany Donner <tiffany.donner@stsupport.se>
2020-04-26 13:33:33 +0000

    Heroku reinstall

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'paw_purfect_planner'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 
 @app.route('/')
