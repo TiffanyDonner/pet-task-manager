ff160e38da2c3d553998bcfa7c3e432a3a9b3b58
Tiffany Donner <tiffany.donner@stsupport.se>
2020-04-27 06:07:14 +0000

    Created base.html file and updates the events.html file to reflect that

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
-if path.exists("env.py"):
-    import env
+if path.exists("env.py"): 
+    import env
 
 app = Flask(__name__)
 
